4266fc8baee309698d861dda509f4ae2e57a71e3
Ramiro <r.escuderogimenez@gmail.com>
2023-01-10 12:07:31 -0300

    finished the modal component with some css fixes

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ElementRef, OnInit } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 
 //separation of responsibilities of the modal components: this component, pertaining to the shared module
@@ -10,12 +10,18 @@ import { ModalService } from 'src/app/services/modal.service';
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css']
 })
-export class ModalComponent {
+export class ModalComponent implements OnInit {
   // this property acts as the input of the value we pass down by the more specific modal as ID
   @Input() modalID = '';
   
-  constructor (public modal: ModalService) {
+  constructor (public modal: ModalService, public el: ElementRef) {
 
+  }
+
+  //moving the modal to the body to solve some CSS issues
+
+  ngOnInit(): void {
+    document.body.appendChild(this.el.nativeElement)
   }
 
   //closes the modal, doesn't need event handling as we're working with div elements only
