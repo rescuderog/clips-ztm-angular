3db87a5b09d6209352a24b29bf51a4e9158e4e17
Ramiro <r.escuderogimenez@gmail.com>
2023-01-11 03:56:25 -0300

    started to implement reactive forms into register

---
+++ src/app/shared/input/input.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.css']
+})
+export class InputComponent {
+  //we create a form control to receive the values passed down by the form
+  @Input() control: FormControl = new FormControl();
+}
---
+++ src/app/user/register/register.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+  //we register the form via the FormGroup object as FormControl objects with their corresponding Validators
+  name = new FormControl('', [
+    Validators.required, 
+    Validators.minLength(3)
+  ]);
+  email = new FormControl('');
+  age = new FormControl('');
+  password = new FormControl('');
+  confirm_password = new FormControl('');
+  phoneNumber = new FormControl('');
+
+  registerForm = new FormGroup({
+    name: this.name,
+    email: this.email,
+    age: this.age,
+    password: this.password,
+    confirm_password: this.confirm_password,
+    phoneNumber: this.phoneNumber
+  });
+}
