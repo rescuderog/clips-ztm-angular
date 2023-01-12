6e0bcc530c276d5e5ff08b66565f9bed466156da
Ramiro <ramiro000@gmail.com>
2023-01-11 23:35:17 -0300

    finished login form via Template Forms

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+//note: the login form was made using Template Forms, so that's why we don't have much on the class
+//it's all in the html file
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  //object to store the credentials
+  credentials = {
+    email: '',
+    password: ''
+  }
 
+  //login function, called from the form, TODO: backend
+  login() {
+    console.log(this.credentials);
+  }
 }
