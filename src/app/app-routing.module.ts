9572b8a304ba1399ede5a3a1708eefce3c1aa22c
Ramiro <ramiro000@gmail.com>
2023-01-18 17:03:38 -0300

    first pass of routing, and more decoupling content

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+//this is a routing module for the app where we config all the paths and corresponding components that
+//angular should render
+
+const routes: Routes = [
+  //home component routing at /. Contains intro and list of videos
+  {
+    path: '',
+    component: HomeComponent
+  },
+  //about component routing at /about. Contains a small about us page.
+  {
+    path: 'about',
+    component: AboutComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
